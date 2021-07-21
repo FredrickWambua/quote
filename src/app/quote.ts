1ea9e9a23cc4aad824c447e40ea25dff64eb825e
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-07-21 20:03:37 +0300

    deletes unwanted comment

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,5 @@
 export class Quote {
-    // showDescription:boolean;
     constructor( public name:string, public author:string, public quotemsg:string, dateposted:Date, public like:number, public dislike:number){
-        // this.showDescription = false;
     }
 
 }
