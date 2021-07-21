8357ead75ef1894f1a60efcc75b44177877b40c3
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-07-21 10:27:43 +0300

    adds likes properties

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     // showDescription:boolean;
-    constructor( public name:string, public author:string, public quotemsg:string, dateposted:Date){
+    constructor( public name:string, public author:string, public quotemsg:string, dateposted:Date, public like:number, public dislike:number){
         // this.showDescription = false;
     }
 
