0dac3eba07f17e43828d30d887b54f2f624a1b8c
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-07-21 08:56:08 +0300

    adds delete method

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-quote-detail',
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
-
+  
+  @Output() isRead = new EventEmitter<boolean>()
+  quoteDelete(read:boolean){
+     this.isRead.emit(read);
+   }
   constructor() { }
 
   ngOnInit(): void {
