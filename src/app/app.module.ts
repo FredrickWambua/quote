b04856227bbbc34e849e724f1b107955cc91b785
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-07-21 10:22:54 +0300

    updates module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { QuoteCreatorComponent } from './quote-creator/quote-creator.component';
 import { FormComponent } from './form/form.component';
 import { FormsModule } from '@angular/forms';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteCreatorComponent,
     FormComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
