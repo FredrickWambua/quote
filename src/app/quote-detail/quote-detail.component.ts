ecd63bfbf1189d7eeb01ecc8577e498df1e17caf
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-07-21 12:20:33 +0300

    adds like and dislike functions

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-detail',
@@ -6,14 +7,25 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
-  
+
   @Output() isRead = new EventEmitter<boolean>()
   quoteDelete(read:boolean){
      this.isRead.emit(read);
    }
+  
+  upVote = 0;
+  like(){
+    this.upVote ++
+  }
+  
+  downVote = 0;
+  dislike(){
+    this.downVote++
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
 }
